0c85b55b2644b82d8a3246eeca284b12959256d7
svg-den <sundaywanjiru@gmail.com>
2024-02-27 13:14:20 +0300

    Done assignment

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,13 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num=num + 2    
     return num
+from ipdb_debugging import plus_two
+
+class TestIpdbDebugging:
+    '''ipdb_debugging.py'''
+    
+    def test_adds_two(self):
+        '''adds_two() adds 2 to input arg and returns sum.'''
+        assert(plus_two(3) == 5)
